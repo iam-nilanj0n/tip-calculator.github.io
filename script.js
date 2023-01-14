73a033a2d9b57f09a3f1846bbac92d1776eac024
iam-nilanj0n <98917336+iam-nilanj0n@users.noreply.github.com>
2023-01-15 03:04:35 +0530

    Complete

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ const per15 = document.querySelector('.tip15');
 const per25 = document.querySelector('.tip25');
 const per50 = document.querySelector('.tip50');
 const resetButton = document.querySelector('.RESET')
+// output para is here
+const tipOutput = document.querySelector('.tipOutput');
+const totalOutput = document.querySelector('.totalOutput');
+// important variable for this code
 let arr = [];
 let tipValue = 0;
+
 // addEventListeners are here
 billInput.addEventListener('change', (event) => {
     billInput.value = event.target.value;
@@ -23,19 +28,32 @@ customInput.addEventListener('change', (event) => {
     customInput.value = event.target.value;
 })
 per5.addEventListener('click', (event)=>{
-    tipValue = 5
+    tipValue = 5;
+    customInput.value = null;
 })
 per10.addEventListener('click', (event)=>{
-    tipValue = 10
+    tipValue = 10;
+    customInput.value = null;
 })
 per15.addEventListener('click', (event)=>{
-    tipValue = 15
+    tipValue = 15;
+    customInput.value = null;
 })
 per25.addEventListener('click', (event)=>{
-    tipValue = 25
+    tipValue = 25;
+    customInput.value = null;
 })
 per50.addEventListener('click', (event)=>{
-    tipValue = 50
+    tipValue = 50;
+    customInput.value = null;
+})
+
+resetButton.addEventListener('click', (event)=>{
+    billInput.value = null;
+    noOfPeopleInput.value = null;
+    tipValue = 0;
+    tipOutput.textContent = '$0.00';
+    totalOutput.textContent = '$0.00';
 })
 // setInterval is here
 const inputPaddingLeftDecrease = setInterval(() => {
@@ -58,8 +76,17 @@ const inputPaddingLeftDecrease = setInterval(() => {
     
     if(customInput.value>0){
         tipValue = customInput.value;
-        console.log(tipValue);
     }
+
+    if(Number(billInput.value)>0 && Number(noOfPeopleInput.value)>0 && tipValue>0){
+        let bill = Number(billInput.value);
+        let tipAmount = (bill*(tipValue/100))/Number(noOfPeopleInput.value);
+        let totalAmount = (bill + (bill*(tipValue/100))/Number(noOfPeopleInput.value))/Number(noOfPeopleInput.value) ;
+
+        tipOutput.textContent =`$${tipAmount.toFixed(2)}`;
+        totalOutput.textContent = `$${totalAmount.toFixed(2)}`;
+    }
+
 }, 50)
 
 
